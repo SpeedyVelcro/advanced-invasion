7d3138860e16a6218a913f55644a81e356d4798f
SpeedyVelcro <32414336+SpeedyVelcro@users.noreply.github.com>
2026-07-23 06:10:45 +0100

    Migrate GameJoltIntegration

--- AutoLoad/GameJoltIntegration/GameJoltIntegration.gd
+++ AutoLoad/GameJoltIntegration/GameJoltIntegration.gd
@@ -1,6 +1,5 @@
 extends Node
 
-@onready var game_jolt_api = $GameJoltAPI
 @onready var ping_timer = $PingTimer
 @onready var secret = $GameJoltSecret
 const YIELD_ENABLED = true
@@ -19,34 +18,39 @@ signal session_open_fail
 signal session_closed
 
 func _ready():
-	game_jolt_api.init(secret.PRIVATE_KEY, secret.GAME_ID)
+	# TODO: outdated
+	# game_jolt_api.init(secret.PRIVATE_KEY, secret.GAME_ID)
 	AchievementManager.connect("achievement_synced", Callable(self, "_on_AchievementManager_achievement_synced"))
 
 func login(username : String, token : String):
-	game_jolt_api.auth_user(username, token)
-	if YIELD_ENABLED:
-			await game_jolt_api.gamejolt_request_completed
-	game_jolt_api.open_session()
+	pass # TODO: outdated
+	#game_jolt_api.auth_user(username, token)
+	#if YIELD_ENABLED:
+			#await game_jolt_api.gamejolt_request_completed
+	#game_jolt_api.open_session()
 
 func login_auto():
-	game_jolt_api.auto_auth()
-	if YIELD_ENABLED:
-			await game_jolt_api.gamejolt_request_completed
-	game_jolt_api.open_session()
+	pass # TODO: outdated
+	#game_jolt_api.auto_auth()
+	#if YIELD_ENABLED:
+			#await game_jolt_api.gamejolt_request_completed
+	#game_jolt_api.open_session()
 
 func logout():
 	set_integration_enabled(false)
-	game_jolt_api.close_session()
+	# TODO: outdated
+	# game_jolt_api.close_session()
 	# Just in case closing session doesn't work we also just straight up stop pinging
 	ping_timer.stop()
 
 func _on_AchievementManager_achievement_synced(achievement_id):
-	if integration_enabled:
-		game_jolt_api.set_trophy_achieved(GAME_JOLT_TROPHY_ID[achievement_id])
-		if YIELD_ENABLED:
-			await game_jolt_api.gamejolt_request_completed
-			# Could handle errors with var result = yield(...)
-			# May not really be necessary though if GameJoltAPI is on verbose.
+	pass # TODO: outdated
+	#if integration_enabled:
+		#game_jolt_api.set_trophy_achieved(GAME_JOLT_TROPHY_ID[achievement_id])
+		#if YIELD_ENABLED:
+			#await game_jolt_api.gamejolt_request_completed
+			## Could handle errors with var result = yield(...)
+			## May not really be necessary though if GameJoltAPI is on verbose.
 
 func _on_GameJoltAPI_gamejolt_request_completed(type, message, finished):
 	match type:
@@ -62,17 +66,20 @@ func _on_GameJoltAPI_gamejolt_request_completed(type, message, finished):
 			emit_signal("session_closed")
 
 func _on_PingTimer_timeout():
-	game_jolt_api.ping_session()
+	pass # TODO: outdated
+	# game_jolt_api.ping_session()
 
 func _on_GameJoltAPI_tree_exiting():
-	if integration_enabled:
-		game_jolt_api.close_session()
+	pass # TODO: outdated
+	#if integration_enabled:
+		#game_jolt_api.close_session()
 
 # Getters and setters
 func set_integration_enabled(value : bool):
 	integration_enabled = value
-	if not value:
-		game_jolt_api.close_session()
+	# TODO: outdated
+	#if not value:
+		#game_jolt_api.close_session()
 
 func is_integration_enabled():
 	return integration_enabled

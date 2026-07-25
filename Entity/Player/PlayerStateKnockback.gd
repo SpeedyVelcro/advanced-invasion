2463586c0bda755e9bc9707b145bdabf5a05ef2a
SpeedyVelcro <32414336+SpeedyVelcro@users.noreply.github.com>
2026-07-25 15:59:26 +0100

    Fix touch virus knockback

--- Entity/Player/PlayerStateKnockback.gd
+++ Entity/Player/PlayerStateKnockback.gd
@@ -12,16 +12,16 @@ func _on_state_enter():
 	#print("entered knockback")
 
 func _physics_process(_delta):
-	var velocity = get_velocity.call_func()
+	var velocity = get_velocity.call()
 	velocity.x -= drag_x * sign(velocity.x)
-	set_velocity.call_func(velocity)
+	set_velocity.call(velocity)
 	print(velocity.x)
 	if velocity.x > -6 and velocity.x < 6:
 		#print("reverting state")
 		revert_state()
 
 func start_new_knockback(knockback_velocity):
-	set_velocity.call_func(knockback_velocity)
+	set_velocity.call(knockback_velocity)
 	disable_snap(false) # Don't need to follow floor in knockback mode
 	#print("started knockback with velocity" + String(knockback_velocity))
 	#print("The player's velocity is now " + String(get_velocity.call_func()))

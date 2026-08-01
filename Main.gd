2dab1ba0a572a0c1803545b3e7bfd8d6bc48ee01
SpeedyVelcro <32414336+SpeedyVelcro@users.noreply.github.com>
2026-08-01 16:54:17 +0100

    Add TODO comment

--- Main.gd
+++ Main.gd
@@ -10,6 +10,7 @@ func _ready():
 	# Workaround because default UI scale doesn't calculate correctly when calculated
 	# before startup. Should fix this problem upstream but this is a temporary
 	# workaround for now.
+	# TODO: upstream fix
 	OptionsProvider.set_default_options(OptionsConfigProvider.get_config().get_default_options())
 	
 	SceneTransition.fade(first_scene_path, 0.0, 1.0)

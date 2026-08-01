186c4619d370aaf75cdb7da0514fee174b6cb5af
SpeedyVelcro <32414336+SpeedyVelcro@users.noreply.github.com>
2026-08-01 16:53:56 +0100

    Fix default UI scale issue

--- Main.gd
+++ Main.gd
@@ -7,4 +7,9 @@ func _ready():
 	
 	OptionsLifecycle.start_up()
 	
+	# Workaround because default UI scale doesn't calculate correctly when calculated
+	# before startup. Should fix this problem upstream but this is a temporary
+	# workaround for now.
+	OptionsProvider.set_default_options(OptionsConfigProvider.get_config().get_default_options())
+	
 	SceneTransition.fade(first_scene_path, 0.0, 1.0)

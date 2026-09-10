224411080f4b18e94d9cdd6397ad4f1b38a427d8
SpeedyVelcro <32414336+SpeedyVelcro@users.noreply.github.com>
2026-09-10 16:28:34 +0100

    Apply upstream default UI scale fix and remove workaround

--- Main.gd
+++ Main.gd
@@ -19,12 +19,6 @@ func _ready():
 	
 	OptionsLifecycle.start_up()
 	
-	# Workaround because default UI scale doesn't calculate correctly when calculated
-	# before startup. Should fix this problem upstream but this is a temporary
-	# workaround for now.
-	# TODO: upstream fix
-	OptionsProvider.set_default_options(OptionsConfigProvider.get_config().get_default_options())
-	
 	if OS.has_feature("newgrounds"):
 		ProjectSettings.set_setting("newgrounds.io/app_id", Secrets.NEWGROUNDS_APP_ID)
 		ProjectSettings.set_setting("newgrounds.io/AES-128_key", Secrets.NEWGROUNDS_AES_128_ENCRYPTION_KEY)
